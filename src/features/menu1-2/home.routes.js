ead054447c3651c86f62a943f56e33352e11108c
zhuwei <zhuwei@zhuwei.com>
2019-11-12 16:50:33 +0800

    添加上层菜单控制

--- src/features/menu1-2/home.routes.js
+++ src/features/menu1-2/home.routes.js
@@ -1,8 +1,8 @@
 routes.$inject = ['$stateProvider'];
 
 export default function routes($stateProvider) {
-  $stateProvider.state('menu1-2', {
-    url: '/menu1-2',
+  $stateProvider.state('menu1-2-1', {
+    url: '/menu1-2-1',
     template: require('./home.html')
   })
 }
