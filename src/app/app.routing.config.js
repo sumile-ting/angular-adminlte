c30b78ce933bef7ddf31ef250fc41c8727dcd243
sumile-ting <540201731@qq.com>
2019-11-14 20:31:46 +0800

    默认路由地址从配置中读取

--- src/app/app.routing.config.js
+++ src/app/app.routing.config.js
@@ -1,7 +1,19 @@
+import config from './menu.config';
+
 routing.$inject = ['$urlRouterProvider', '$locationProvider'];
 
 export default function routing($urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise('/menu1-1-1');
+
+  if(config.length > 0 && config[0].subMenu.length > 0) {
+    let subConf = config[0].subMenu[0];
+    if(subConf.isLeaf) {
+      $urlRouterProvider.otherwise(subConf.href);
+    } else if(subConf.subMenu && subConf.subMenu.length > 0){
+      $urlRouterProvider.otherwise(subConf.subMenu[0].href)
+    } else {
+      $urlRouterProvider.otherwise('/');
+    }
+  }
   $locationProvider.html5Mode(true);
 }
 
